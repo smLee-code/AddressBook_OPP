1c462d6a59de52910cdb89d5d6f403c82c9f8dbd
leesm4510@gmail.com <leesm4510@gmail.com>
2022-05-21 21:11:48 +0900

    C++ 단일 연결 리스트 : step 8 풀이

--- UserInterface.cpp
+++ UserInterface.cpp
@@ -15,22 +15,60 @@ CUserInterface::~CUserInterface()
 
 void CUserInterface::Add()
 {
+	char szName[32] = { 0, };
+	char szPhone[32] = { 0, };
 
+	cout << "Input name : ";
+	cin >> szName;
+
+	cout << "Input phone number : ";
+	cin >> szPhone;
+
+	m_List.AddNewNode(szName, szPhone);
 }
 
 void CUserInterface::Search()
 {
+	char szName[32];
+	CUserData* pNode = NULL;
 
+	cout << "Input name : ";
+	cin >> szName;
+
+	pNode = m_List.FindNode(szName);
+	if (pNode != NULL)
+	{
+		printf("[%p] %s\t%s [%p]\n",
+			pNode,
+			pNode, pNode->GetName(), 
+			pNode->GetPhone(),
+			pNode->GetNext());
+	}
+	else
+	{
+		cout << "ERROR: 데이터를 찾을 수 없습니다." << endl;
+	}
 }
 
 void CUserInterface::Remove()
 {
+	char szName[32];
 
+	cout << "Input name : ";
+	cin >> szName;
+
+	m_List.RemoveNode(szName);
 }
 
 int CUserInterface::PrintUI()
 {
+	int nInput = 0;
 
+	system("cls");
+	cout << "[1] Add\t [2] Search\t [3] Print all\t [4] Remove\t [0] Exit\n:";
+	cin >> nInput;
+
+	return nInput;
 }
 
 int CUserInterface::Run()
@@ -47,7 +85,7 @@ int CUserInterface::Run()
 		case 2:
 			Search();
 		case 3:
-			m_List.PrintAll();	// 원문 : PrintAll(); - 컴파일 오류로 수정
+			m_List.PrintAll();
 			break;
 		case 4:
 			break;
